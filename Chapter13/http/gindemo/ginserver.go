3f89c6ea8c9cf9273293cea7bf1334d42f5e4ffa
alanhou <info@iscbj.com>
2020-12-20 11:41:25 +0800

    http及其他标准库

---
+++ Chapter13/http/gindemo/ginserver.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+const keyRequestId = "requestId"
+
+func main() {
+	r := gin.Default()
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	r.Use(func(c *gin.Context) {
+		s := time.Now()
+		c.Next()
+
+		// path, status, log latency
+		logger.Info("incoming request",
+			zap.String("path", c.Request.URL.Path),
+			zap.Int("status", c.Writer.Status()),
+			zap.Duration("elapsed", time.Now().Sub(s)),
+		)
+	}, func(c *gin.Context) {
+		c.Set(keyRequestId, rand.Int())
+		c.Next()
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		h := gin.H{
+			"message": "pong",
+		}
+		if rid, exists := c.Get(keyRequestId); exists {
+			h[keyRequestId] = rid
+		}
+		c.JSON(200, h)
+	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello")
+	})
+	r.Run()
+}
